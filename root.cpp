0deba43f2d52696d4328b7b8cb46642883223532
AccelRanger <165313589+AccelRanger@users.noreply.github.com>
2025-05-18 12:58:03 +0200

    Create root.cpp

---
+++ root.cpp
@@ -0,0 +1,85 @@
+// AccelSystems // LU18/05/25//
+
+// Config
+
+// Constants
+const int enA = 11;
+const int inA1 = 12;
+const int inA2 = 13;
+
+const int relay = 2;
+
+// Variables
+int valvePosition = 0;
+
+// Joystick pins
+const int VRx = A0;
+const int VRy = A1;
+const int SW = A2;
+
+// Functions
+
+void killValveMotors() {
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, LOW);
+}
+
+void setValveSpeed(int speed) {
+  analogWrite(enA, speed);
+}
+
+void valve(String state) {
+  Serial.print("returning valve");
+  if (state == "open") {
+    digitalWrite(inA1, HIGH);
+    digitalWrite(inA2, LOW);
+    valvePosition += 1;
+    delay(500);
+    killValveMotors();
+  }
+  else if (state == "close") {
+    digitalWrite(inA1, LOW);
+    digitalWrite(inA2, HIGH);
+    valvePosition -= 1;
+    delay(500);
+    killValveMotors();
+  }
+}
+
+void compressor(const char* state) {
+  Serial.print("returning compressor");
+
+  if (strcmp(state, "/") == 0) { // latching logic
+    digitalWrite(relay, HIGH);
+  }
+  else if (strcmp(state, "//") == 0) {
+    digitalWrite(relay, LOW);
+  }
+}
+
+// Setup
+
+void setup() {
+  // Driver pins
+  pinMode(enA, OUTPUT);
+  pinMode(inA1, OUTPUT);
+  pinMode(inA2, OUTPUT);
+  
+  // Relay pin
+  pinMode(relay, OUTPUT);
+  
+  // Joystick setup
+  pinMode(SW, INPUT_PULLUP);
+
+  // Start serial communication
+  Serial.begin(9600);
+
+  setValveSpeed(15);
+  valve("open");
+}
+
+// Loop
+
+void loop() {
+  
+}
